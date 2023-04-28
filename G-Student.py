6100ffb01235754ea3355133873a5376a29f50a1
macsurat <macsurat@gmail.com>
2023-04-28 09:25:35 +0700

    Show running

--- G-Student.py
+++ G-Student.py
@@ -8,7 +8,7 @@ sc = pyspark.SparkContext()
 
 spark = SparkSession(sc)
 
-server_name = "jdbc:sqlserver://104.199.184.154"
+server_name = "jdbc:sqlserver://34.16.141.112"
 database_name = "inclassdb"
 url = server_name + ";" + "databaseName=" + database_name + ";"
 
